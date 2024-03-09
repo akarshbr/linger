1ab66cd7cad132fb6037ef8411b6a9a147f126e8
akarshbr <akarshbrajeev@gmail.com>
2024-03-09 22:03:28 +0530

    login screen added

---
+++ lib/view/login_screen/login_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+import '../../controller/sql_helper.dart';
+
+class HotelLogin extends StatelessWidget {
+  HotelLogin({super.key});
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    void loginUser(String email, String password) async {
+      if (email == "@admin" && password == "root123") {
+        //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HotelAdmin()));
+      } else {
+        var data = await SQLFunction.checkUserExist(email, password);
+        if (data.isNotEmpty) {
+          //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HotelHome(data: data,)));
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("Invalid Email or password"),
+            backgroundColor: Colors.redAccent,
+          ));
+        }
+      }
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Form(
+        key: formkey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "LOGIN",
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+            ),
+            Image(
+              image: AssetImage("assets/hotel/hotellogo.png"),
+              height: 100,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: TextFormField(
+                controller: emailController,
+                validator: (enteredEmail) {
+                  if (enteredEmail!.isEmpty || !enteredEmail.contains("@")) {
+                    return "Enter Proper Email Idiot";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                  hintText: "Email",
+                  hintStyle: TextStyle(color: Colors.purple),
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: Colors.purple),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+              child: TextFormField(
+                controller: passwordController,
+                validator: (enteredPassword) {
+                  if (enteredPassword!.isEmpty || enteredPassword.length < 6) {
+                    return "Password length should be greater than 6";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                  hintText: "Password",
+                  hintStyle: TextStyle(color: Colors.purple),
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: Colors.purple),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                var valid = formkey.currentState!.validate();
+                if (valid == true) {
+                  loginUser(emailController.text, passwordController.text);
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text("Please Verify All the Fields"),
+                    backgroundColor: Colors.redAccent,
+                  ));
+                }
+              },
+              child: Text(
+                "Login",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+                  // If the button is pressed, return green, otherwise blue
+                  if (states.contains(MaterialState.pressed)) {
+                    return Colors.white;
+                  }
+                  return Colors.purple;
+                }),
+              ),
+            ),
+            Wrap(
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                Text("Dont have an Account,"),
+                TextButton(
+                    onPressed: () {
+                      //Navigator.push(context, MaterialPageRoute(builder: (context) => HotelSignup()));
+                    },
+                    child: Text(
+                      "Sign UP",
+                      style: TextStyle(color: Colors.purple),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
